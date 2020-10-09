1bd5181b5932a395fe2f387719445234b513e721
ClancyLight <clancylight@gmail.com>
2020-10-09 23:10:58 +1300

    reminder to exclude from build

--- stm32f4xx_drivers/Src/main.c
+++ stm32f4xx_drivers/Src/main.c
@@ -3,6 +3,11 @@
  *
  *  Created on: 14/09/2020
  *      Author: clancytownsend
+ *
+ *      NOTE: Don't forget to exclude from build everything except:
+ *      	- syscalls.c
+ *      	- sysmem.c
+ *      	- the main.c of your choice (this is for study, not a product)
  */
 
 #include "stm32f411xx.h"
